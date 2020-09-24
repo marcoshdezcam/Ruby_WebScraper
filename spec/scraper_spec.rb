f5bb505e4e44a1b72fbaa706e29ee6899a47e163
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-24 15:46:32 -0500

    Change test descriptions according to best practices

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,23 +1,21 @@
 require_relative '../lib/scraper.rb'
 
 describe Scraper do
-  context %(Intial Scraper distributors data) do
+  context %(When the Scraper object is created) do
     it %(has a hash with the initial distributors data) do
       scraper = Scraper.new('CPU')
       expect(scraper.distributors).to be_instance_of(Hash)
     end
-  end
-  context %(When the Scraper is created, it should have keywords asigned.) do
-    it %(has keywords to search) do
+    it %(has initial keywords to search) do
       scraper = Scraper.new('RAM 8GB')
       expect(scraper.keywords.empty?).to be false
     end
   end
   context %(When a search is done) do
-    it %( creates a Mechanize object on the results variable) do
+    it %(saves the results on the instance variable results) do
       scraper = Scraper.new('RAM 8GB')
       scraper.search
-      expect(scraper.results).to be_instance_of(Mechanize)
+      expect(scraper.results.empty?).to be false
     end
   end
 end
