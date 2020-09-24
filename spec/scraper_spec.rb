fb9e566c698c92ed31365fab17e9793112bde671
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-23 23:03:43 -0500

    Add test to verify initial distributors data
    Check if the initial distributors data is correctly arranged in a Hash
    in Scraper class

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,11 +1,15 @@
+require_relative '../lib/scraper.rb'
+
 describe Scraper do
-  context %(A distributors list exists) do
-    it %(has a hash with the distributors data) do
+  context %(Intial Scraper distributors data) do
+    it %(has a hash with the initial distributors data) do
+      scraper = Scraper.new('CPU')
+      expect(scraper.distributors).to be_instance_of(Hash)
     end
   end
   context %(When the Scraper is created, it should have keywords asigned.) do
     it %(has keywords to search) do
-      scraper = Scraper.new('RAM, 8GB')
+      scraper = Scraper.new('RAM 8GB')
       expect(scraper.keywords.empty?).to be false
     end
   end
