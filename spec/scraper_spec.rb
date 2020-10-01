c0f94219b6fd1efb79d0c18bdfb9a3694c1feb43
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-10-01 12:36:26 -0500

    Refactor tests to use let
    Added creation of object with let at the beginning of the tests, so an
    object isn't created on each test

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,25 +1,22 @@
 require_relative '../lib/scraper.rb'
 
 describe Scraper do
+  let(:scraper) { Scraper.new('RAM 8GB') }
   context %(When the Scraper object is created) do
     it %(has a hash with the initial distributors data) do
-      scraper = Scraper.new('CPU')
       expect(scraper.distributors).to be_instance_of(Hash)
     end
     it %(has initial keywords to search) do
-      scraper = Scraper.new('RAM 8GB')
       expect(scraper.keywords.empty?).to be false
     end
   end
   context %(When a search is done) do
     it %(saves the results on the instance variable results) do
-      scraper = Scraper.new('RAM 8GB')
       scraper.search
       expect(scraper.results.empty?).to be false
     end
   end
   context %(When searching on each distributor, return true or false to report retults) do
-    let(:scraper) { Scraper.new('RAM 8GB') }
     it { expect(scraper.mercadolibre).to be(true).or be(false) }
     it { expect(scraper.cyberpuerta).to be(true).or be(false) }
     it { expect(scraper.pchmayoreo).to be(true).or be(false) }
@@ -37,7 +34,6 @@ describe Scraper do
   end
   describe %(#show_distributors) do
     it %(returns the hash of distributors) do
-      scraper = Scraper.new('RAM 8GB')
       expect(scraper.show_distributors).to be(scraper.distributors)
     end
   end
