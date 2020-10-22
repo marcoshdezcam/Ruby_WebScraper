705adbf4881b2a5c628524e4205e7e1eb4af35e7
Marcos Hernández <marcoshdezcam@gmail.com>
2020-10-22 09:18:42 -0500

    Create Product objects instead of temp Scraper.results array
    Instead of using Scraper.results to temporarily save the search results given by the scraper methods, an object is created on each iteration of the search.

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,17 @@
 class Product
-  attr_reader :name, :price, :url
+  attr_accessor :name, :price, :url
 
   def initialize(name, price, url)
-    @name = name
-    @price = price
+    @name = clean_string(name)
+    @price = clean_string(price)
     @url = url
+  end
+
+  def clean_string(string)
+    string.tr!("\t", '')
+    string.tr!("\n", ',')
+    string.tr!("\r", '')
+    string.strip!
+    string
   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'selenium-webdriver'
 
 class Scraper
+  attr_accessor :listing
   attr_reader :agent, :chrome, :results, :keywords, :distributors
 
   def initialize(keywords)
@@ -12,7 +13,7 @@ class Scraper
     chrome_options = Selenium::WebDriver::Chrome::Options.new
     chrome_options.add_argument('--headless')
     @chrome = Selenium::WebDriver.for :chrome, options: chrome_options
-    @results = [['Product name'], ['Price'], ['URL']]
+    @results = []
     @keywords = keywords
     @distributors = {
       mercadolibre: 'https://www.mercadolibre.com.mx/', cyberpuerta: 'https://www.cyberpuerta.mx/',
@@ -41,55 +42,54 @@ class Scraper
     highpro
     pcdigital
     intercompras
-    clean_results
   end
 
   def amazon
-    no_results = @results[0].size
+    results_before_search = @results.size
     @chrome.navigate.to distributors[:amazon]
     input = @chrome.find_element(name: 'field-keywords')
     input.send_keys @keywords
     input.submit
     @chrome.find_elements(class: 's-result-item').each do |item|
       begin
-        @results[0] << item.find_element(class: 'a-text-normal').text
-        @results[1] << item.find_element(class: 'a-price').text
-        @results[2] << item.find_element(class: 'a-text-normal').attribute('href')
+        @results << Product.new(item.find_element(class: 'a-text-normal').text,
+                                item.find_element(class: 'a-price').text,
+                                item.find_element(class: 'a-text-normal').attribute('href'))
       rescue Selenium::WebDriver::Error::NoSuchElementError
-        break
+        next
       end
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def mercadolibre
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:mercadolibre])
     webpage.forms.first.as_word = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('div.ui-search-result__wrapper').each do |item|
-      @results[0] << item.css('h2.ui-search-item__title').text
-      @results[1] << item.css('span.ui-search-price__part').first.text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('h2.ui-search-item__title').text,
+                              item.css('span.ui-search-price__part').first.text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def cyberpuerta
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:cyberpuerta])
     webpage.form('search').searchparam = @keywords
     results_page = webpage.form('search').submit
     results_page.css('div.emproduct').each do |item|
-      @results[0] << item.css('a.emproduct_right_title').text
-      @results[1] << item.css('label.price').text
-      @results[2] << item.css('a.emproduct_right_title').first['href']
+      @results << Product.new(item.css('a.emproduct_right_title').text,
+                              item.css('label.price').text,
+                              item.css('a.emproduct_right_title').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def pchmayoreo
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:pchmayoreo])
     login_page = webpage.link_with(text: 'Iniciar Sesión').click
     login_form = login_page.form_with(id: 'login-form')
@@ -100,178 +100,168 @@ class Scraper
     search_form.q = @keywords
     results_page = agent.submit(search_form)
     results_page.css('div.item-inner').each do |item|
-      @results[0] << item.css('h2.product-name').text
-      @results[1] << item.css('span.price').first.text
-      @results[2] << item.css('h2.product-name a').first['href']
+      @results << Product.new(item.css('h2.product-name').text,
+                              item.css('span.price').first.text,
+                              item.css('h2.product-name a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def mipc
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:mipc])
     webpage.form_with(id: 'search_mini_form').q = @keywords
     results_page = webpage.form_with(id: 'search_mini_form').submit
     results_page.css('li.product-item').each do |item|
-      @results[0] << item.css('h5.product-item-name').text
-      @results[1] << item.at('[data-price-type="finalPrice"]').text
-      @results[2] << item.css('a.product-item-link').first['href']
+      @results << Product.new(item.css('h5.product-item-name').text,
+                              item.at('[data-price-type="finalPrice"]').text,
+                              item.css('a.product-item-link').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def orbitalstore
-    no_results = @results[0].size
+    results_before_search = @results.size
     results_page = @agent.get(distributors[:oribalstore] + @keywords)
     results_page.css('div.item').each do |item|
-      @results[0] << item.css('a.title').text
-      @results[1] << item.css('div.played').text
-      @results[2] << item.css('a.title').first['href']
+      @results << Product.new(item.css('a.title').text,
+                              item.css('div.played').text,
+                              item.css('a.title').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def grupodecme
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:grupodecme])
     webpage.forms.first.q = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('a.product-grid-item').each do |item|
-      @results[0] << item.css('p').text
-      @results[1] << item.css('span.visually-hidden')[1].text
-      @results[2] << (distributors[:grupodecme] + item['href'])
+      @results << Product.new(item.css('p').text,
+                              item.css('span.visually-hidden')[1].text,
+                              (distributors[:grupodecme] + item['href']))
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def digitalife
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:digitalife])
     webpage.form_with(class: 'buscador form-inline text-center').term = @keywords
     results_page = webpage.form_with(class: 'buscador form-inline text-center').submit
     results_page.css('div.productoInfoBloq').each do |item|
-      @results[0] << item.css('span.tituloHighlight').text
-      @results[1] << item.css('div.precioFlag').text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('span.tituloHighlight').text,
+                              item.css('div.precioFlag').text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def pcel
-    no_results = @results[0].size
+    results_before_search = @results.size
     @chrome.navigate.to distributors[:pcel]
     input = @chrome.find_element(name: 'filter_name')
     input.send_keys @keywords
     chrome.find_element(class: 'button-search').click
     results_page = @agent.get(@chrome.current_url)
     results_page.css('tr').each do |item|
-      @results[0] << item.css('div.name').text[0...35] unless item.css('div.name').empty?
-      @results[1] << item.css('span.price-new').text unless item.css('div.name').empty?
-      @results[2] << item.css('a').first['href'] unless item.css('div.name').empty?
+      next if item.css('div.name').empty?
+
+      @results << Product.new(item.css('div.name').text,
+                              item.css('span.price-new').text,
+                              item.css('div.name a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def ddtech
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:ddtech])
     webpage.forms.first.search = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('div.item').each do |item|
-      @results[0] << item.css('a').text
-      @results[1] << item.css('span.price').text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('a').text,
+                              item.css('span.price').text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def zegucom
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:zegucom])
     webpage.forms.first.cons = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('div.search-result').each do |item|
-      @results[0] << item.css('div.result-description a').text
-      @results[1] << item.css('span.result-price-search').text
-      @results[2] << distributors[:zegucom] + item.css('a')[1]['href']
+      @results << Product.new(item.css('div.result-description a').text,
+                              item.css('span.result-price-search').text,
+                              (distributors[:zegucom] + item.css('a')[1]['href']))
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def pcmig
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:pcmig])
     webpage.forms.first.s = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('div.product-wrapper').each do |item|
-      @results[0] << item.css('h2.product-name').first.text
-      @results[1] << item.css('span.woocommerce-Price-amount').first.text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('h2.product-name').first.text,
+                              item.css('span.woocommerce-Price-amount').first.text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def highpro
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:highpro])
     webpage.form_with(id: 'searchbox').search_query = @keywords
     results_page = webpage.form_with(id: 'searchbox').submit
     results_page.css('div.product-container').each do |item|
-      @results[0] << item.css('h5.product-title-item').text
-      @results[1] << item.css('div.product-price-and-shipping').text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('h5.product-title-item').text,
+                              item.css('div.product-price-and-shipping').text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def pcdigital
-    no_results = @results[0].size
+    results_before_search = @results.size
     @chrome.navigate.to distributors[:pcdigital]
     input = @chrome.find_element(name: 'search')
     input.send_keys @keywords
     @chrome.find_element(class: 'button-search').click
     results_page = agent.get(@chrome.current_url)
     results_page.css('div.product').each do |item|
-      @results[0] << item.css('div.name').text
-      @results[1] << item.css('span.price-new').text
-      @results[2] << item.css('div.name a').first['href']
+      @results << Product.new(item.css('div.name').text,
+                              item.css('span.price-new').text,
+                              item.css('div.name a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
   def intercompras
-    no_results = @results[0].size
+    results_before_search = @results.size
     webpage = @agent.get(distributors[:intercompras])
     webpage.forms.first.keywords = @keywords
     results_page = webpage.forms.first.submit
     results_page.css('div.divContentProductInfo').each do |item|
-      @results[0] << item.css('a.spanProductListInfoTitle').text
-      @results[1] << item.css('div.divProductListPrice').text
-      @results[2] << item.css('a').first['href']
+      @results << Product.new(item.css('a.spanProductListInfoTitle').text,
+                              item.css('div.divProductListPrice').text,
+                              item.css('a').first['href'])
     end
-    @results[0].size > no_results
+    @results.size > results_before_search
   end
 
-  def clean_results
-    @results.each do |parameter|
-      parameter.each do |item|
-        item.tr!("\t", '')
-        item.tr!("\n", '')
-        item.tr!("\r", '')
-        item.strip!
-      end
-    end
-  end
-
-  def show_results
-    @results
+  def show_distributors
+    @distributors
   end
 
   def create_listing
-    listing_results = Listing.new(@results)
+    @listing = Listing.new(@results)
   end
 end
 
 scraping_test = Scraper.new('RAM 16GB')
 scraping_test.search
-scraping_test.create_listing
